a49cc4be828299c470af1c029699c6269a2ce425
Rajat Sharma <lunasunkaiser@gmail.com>
2022-08-10 23:06:18 +0530

    fix markers

--- src/Markers.hs
+++ src/Markers.hs
@@ -1,19 +1,19 @@
 module Markers where
 
 migrationGenerateMarker :: String
-migrationGenerateMarker = "// kensai-generate-marker-migration"
+migrationGenerateMarker = "// vhagar-generate-marker-migration"
 
 entityGenerateMarker :: String
-entityGenerateMarker = "// kensai-generate-marker-entity"
+entityGenerateMarker = "// vhagar-generate-marker-entity"
 
 typeGenerateMarker :: String
-typeGenerateMarker = "# kensai-generate-marker-type"
+typeGenerateMarker = "# vhagar-generate-marker-type"
 
 inputGenerateMarker :: String
-inputGenerateMarker = "# kensai-generate-marker-input"
+inputGenerateMarker = "# vhagar-generate-marker-input"
 
 queryGenerateMarker :: String
-queryGenerateMarker = "# kensai-generate-marker-query"
+queryGenerateMarker = "# vhagar-generate-marker-query"
 
 mutationGenerateMarker :: String
-mutationGenerateMarker = "# kensai-generate-marker-mutation"
+mutationGenerateMarker = "# vhagar-generate-marker-mutation"
